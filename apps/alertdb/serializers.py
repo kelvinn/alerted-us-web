3f7dde1cc7acc2f1693762fe70c9a4419898ca19
Kelvin Nicholson <kelvin@kelvinism.com>
2014-11-07 06:58:12 +1100

    no message

--- apps/alertdb/serializers.py
+++ apps/alertdb/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework_gis import serializers as gis_serializers
 from rest_framework import serializers
 from apps.alertdb.models import Alert, Info, Area, Parameter, Resource
-
+from statsd.defaults.django import statsd
 
 class ResourceSerializer(gis_serializers.GeoModelSerializer):
 
@@ -34,6 +34,7 @@ class InfoSerializer(gis_serializers.GeoModelSerializer):
 class AlertSerializer(gis_serializers.GeoModelSerializer):
     info_set = InfoSerializer(many=True)
 
+    @statsd.timer('api.AlertSerializer.validate_cap_id')
     def validate_cap_id(self, attrs, source):
         """
         Check that the alert does not already exist.
