41a9b5ba97de5f3dfcdc181e16db7e7a369e69e1
Kelvin N <kelvin@kelvinism.com>
2015-02-18 10:13:34 +1100

    Loader tests can't be run at the same time, so moving to sequential

--- tests/performance.py
+++ tests/performance.py
@@ -11,17 +11,20 @@ test_id_2 = getenv('TEST_ID_2', None)
 
 loader = Loaderio(api_key)
 
-result_id = loader.tests.run(test_id)['result_id']
-result_id_2 = loader.tests.run(test_id_2)['result_id']
-
 result = None
-result_2 = None
-
+result_id = loader.tests.run(test_id)['result_id']
 for x in range(0, 300, 10):
     sleep(10)
     result = loader.results.get(test_id,result_id)
+    if result['status'] == 'ready': break
+
+# result_id_2 needs to after the first run, since they can't run at the same time
+result_2 = None
+result_id_2 = loader.tests.run(test_id_2)['result_id']
+for x in range(0, 300, 10):
+    sleep(10)
     result_2 = loader.results.get(test_id,result_id_2)
-    if result['status'] == 'ready' and result_2 == 'ready': break
+    if result_2 == 'ready': break
 
 if result and result_2:
     if result['avg_response_time'] > 1000 < result_2['avg_response_time']:
