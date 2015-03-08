5d793e7ac9dc22c846c942b3910b83be0d4286c2
Kelvin N <kelvin@kelvinism.com>
2015-03-08 22:31:05 +1100

    Only using celery if not on open shift

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
+import os
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+if 'RACK_ENV' in os.environ:
+    if os.environ['RACK_ENV'] != 'openshift':
+        from .celery import app as celery_app
