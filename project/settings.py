a8a76ec63c4746ce6752e8b55122f38e8f66348e
Kelvin N <kelvin@kelvinism.com>
2015-03-08 23:28:15 +1100

    no message

--- project/settings.py
+++ project/settings.py
@@ -58,14 +58,13 @@ if 'RACK_ENV' in os.environ:
     elif os.environ['RACK_ENV'] == "testing":
         ON_SNAP_CI = True
     elif os.environ['RACK_ENV'] == 'openshift':
-        DEBUG = True
         ON_OPENSHIFT = True
 
 # Force debug to be on
-if 'DEBUG' in os.environ:
-    if os.environ['DEBUG'] == 'True':
-        ENABLE_DEBUG_TOOLBAR = True
-        DEBUG = True
+#if 'DEBUG' in os.environ:
+#    if os.environ['DEBUG'] == 'True':
+#        ENABLE_DEBUG_TOOLBAR = True
+#        DEBUG = True
 
 if ON_DO:
     REDIS_PASSWORD = ""
