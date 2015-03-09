559da8690d7ef048e340c17082123c73431bbab0
Kelvin N <kelvin@kelvinism.com>
2015-03-09 18:16:55 +1100

    no message

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,4 @@
 from __future__ import absolute_import
-import os
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-if 'RACK_ENV' in os.environ:
-    if os.environ['RACK_ENV'] != 'openshift':
-        from .celery import app as celery_app
