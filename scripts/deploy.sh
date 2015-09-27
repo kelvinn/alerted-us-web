d4a63ca12e8d8e1d426dd4e0baf810e09b4ecabe
Kelvin N <kelvin@kelvinism.com>
2015-09-28 09:03:49 +1300

    Moved ci tests and deploy to script

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+pip install -U tutum requests loaderio
+tutum service set --image tutum.co/zephell/alerted-us-web:$SNAP_PIPELINE_COUNTER $TUTUM_ENVIRONMENT_NAME --sync
+tutum service redeploy $TUTUM_ENVIRONMENT_NAME --sync
+python tests/smoke.py $DEPLOYED_HOSTNAME
