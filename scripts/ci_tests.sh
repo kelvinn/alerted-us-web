ff196aa3ff2419cb2f9852f89f9ef83cd2e28170
Kelvin N <kelvin@kelvinism.com>
2015-09-28 22:17:19 +1300

    Added SHA to tag

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -5,5 +5,5 @@ sudo docker-compose up -d
 sudo docker-compose run db sh -c 'exec psql -h "$DB_PORT_5432_TCP_ADDR" -p "$DB_PORT_5432_TCP_PORT" -U postgres -c "CREATE EXTENSION IF NOT EXISTS POSTGIS"'
 sudo docker-compose run web ./run_ci_tests.sh
 sudo docker login -e $TUTUM_EMAIL -u $TUTUM_USER -p $TUTUM_PASS tutum.co
-sudo docker tag repo_web tutum.co/zephell/alerted-us-web:$SNAP_PIPELINE_COUNTER
-sudo docker push tutum.co/zephell/alerted-us-web:$SNAP_PIPELINE_COUNTER
+sudo docker tag repo_web tutum.co/zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
+sudo docker push tutum.co/zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
