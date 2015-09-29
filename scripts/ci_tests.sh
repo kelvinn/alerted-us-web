7e1c6d7f419a2207d4607c500dd1e748f5a0889f
Kelvin N <kelvin@kelvinism.com>
2015-09-30 10:16:56 +1300

    Specify which docker-compose version to use

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-ip install -U docker-compose==1.4.2
+pip install -U docker-compose==1.4.2
+
+
 sudo docker-compose up -d
 sudo docker-compose run db sh -c 'exec psql -h "$DB_PORT_5432_TCP_ADDR" -p "$DB_PORT_5432_TCP_PORT" -U postgres -c "CREATE EXTENSION IF NOT EXISTS POSTGIS"'
 sudo docker-compose run web ./run_ci_tests.sh
