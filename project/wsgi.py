9647d3519bde8d164ca693ea67bf83a56422fe8f
Kelvin N <kelvin@kelvinism.com>
2015-10-02 09:13:09 +1300

    Enable new relic in production

--- project/wsgi.py
+++ project/wsgi.py
@@ -18,7 +18,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 sys.path.append(BASE_DIR)
 
 try:
-    if os.environ['RACK_ENV'] == 'staging':
+    if os.environ['RACK_ENV'] in ['staging', 'production']:
         import newrelic.agent
         newrelic.agent.initialize()
 except:
