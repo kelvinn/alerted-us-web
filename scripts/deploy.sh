c4d991e18689d616f9bac66dffafcf76eaa86f68
Kelvin N <kelvin@kelvinism.com>
2015-10-13 09:58:11 +1300

    Add 10s sleep for container to come up.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 
 pip install -U tutum requests loaderio
 tutum service set --image tutum.co/zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TUTUM_ENVIRONMENT_NAME --sync
-tutum service redeploy $TUTUM_ENVIRONMENT_NAME --sync
+tutum service redeploy $TUTUM_ENVIRONMENT_NAME --sync && sleep 10
 python tests/smoke.py $DEPLOYED_HOSTNAME
