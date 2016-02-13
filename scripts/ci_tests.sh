3d7eb0c87b5c4d81fe45042f9a538ad00737f86a
Kelvin N <kelvin@kelvinism.com>
2016-02-13 21:40:29 +1300

    Moving to Docker Hub

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -6,6 +6,6 @@ pip install -U docker-compose==1.4.2
 sudo docker-compose up -d
 sudo docker-compose run db sh -c 'exec psql -h "$DB_PORT_5432_TCP_ADDR" -p "$DB_PORT_5432_TCP_PORT" -U postgres -c "CREATE EXTENSION IF NOT EXISTS POSTGIS"'
 sudo docker-compose run web ./scripts/run_ci_tests.sh
-sudo docker login -e $TUTUM_EMAIL -u $TUTUM_USER -p $TUTUM_PASS tutum.co
-sudo docker tag repo_web tutum.co/zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
-sudo docker push tutum.co/zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
+sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+sudo docker tag repo_web zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
+sudo docker push zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
 pip install -U tutum requests loaderio
-tutum service set --image tutum.co/zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TUTUM_ENVIRONMENT_NAME --sync
+tutum service set --image zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TUTUM_ENVIRONMENT_NAME --sync
 tutum service redeploy $TUTUM_ENVIRONMENT_NAME --sync && sleep 10
 python tests/smoke.py $DEPLOYED_HOSTNAME
