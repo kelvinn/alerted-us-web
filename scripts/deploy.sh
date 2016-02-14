c8706977621f04d4882c61e3948eebcfbe6bb296
Kelvin N <kelvin@kelvinism.com>
2016-02-14 16:38:01 +1300

    Updated CirclCI file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pip install -U tutum requests loaderio
+pip install -U tutum requests==2.7.0 loaderio
 tutum service set --image zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TUTUM_ENVIRONMENT_NAME --sync
 tutum service redeploy $TUTUM_ENVIRONMENT_NAME --sync && sleep 10
 python tests/smoke.py $DEPLOYED_HOSTNAME
