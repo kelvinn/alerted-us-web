4185050db293b6450bcaf9f16a1527e2cc65251c
Kelvin N <kelvin@kelvinism.com>
2016-02-15 23:06:43 +1300

    Pin to exact versions

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 ln -s /usr/lib/python2.7/plat-*/_sysconfigdata_nd.py /usr/lib/python2.7/
-uwsgi_python --enable-threads --single-interpreter --http-socket :8000 --module project.wsgi
+uwsgi --enable-threads --single-interpreter --http :8000 --module project.wsgi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# Call this like deploy.sh some-name-on-docker-cloud https://some-name.com
+
 virtualenv ~/venv
 source ~/venv/bin/activate
 
+TARGET_ENVNAME=$1
+TARGET_HOSTNAME=$2
+
 pip install -U docker-cloud requests==2.7.0
-docker-cloud service set --image zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TUTUM_ENVIRONMENT_NAME --sync
-docker-cloud service redeploy $TUTUM_ENVIRONMENT_NAME --sync && sleep 10
-python tests/smoke.py $DEPLOYED_HOSTNAME
+docker-cloud service set --image zephell/alerted-us-web:$SEMAPHORE_BUILD_NUMBER-$SEMAPHORE_BRANCH_ID  --sync
+docker-cloud service set --image zephell/alerted-us-web:$SNAP_COMMIT_SHORT-$SNAP_PIPELINE_COUNTER $TARGET_ENVNAME --sync
+docker-cloud service redeploy $TARGET_ENVNAME --sync && sleep 10
+python tests/smoke.py $TARGET_HOSTNAME
