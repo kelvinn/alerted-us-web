a67917b25a3b936b4f02ec34e1fd81e2bcd05807
Kelvin N <kelvin@kelvinism.com>
2016-02-16 11:25:09 +1300

    Add sleep. Not like.

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -8,7 +8,10 @@ source ~/venv/bin/activate
 pip install -U docker-compose==1.4.2
 
 docker-compose up -d
+sleep 10 # Wait for DB to come up before proceeding. Can be better...
+
 docker-compose run db sh -c 'exec psql -h "$DB_PORT_5432_TCP_ADDR" -p "$DB_PORT_5432_TCP_PORT" -U postgres -c "CREATE EXTENSION IF NOT EXISTS POSTGIS"'
+
 docker-compose run web /bin/bash -c "python manage.py migrate --noinput"
 docker-compose run web /bin/bash -c "python manage.py collectstatic --noinput"
 docker-compose run web /bin/bash -c "python manage.py test"
