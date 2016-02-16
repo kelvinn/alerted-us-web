0aa1f944be01fffd4b05112d63719974427d6678
Kelvin N <kelvin@kelvinism.com>
2016-02-16 16:39:52 +1300

    Update uwsgi settings

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 ln -s /usr/lib/python2.7/plat-*/_sysconfigdata_nd.py /usr/lib/python2.7/
-uwsgi --enable-threads --single-interpreter --http :8000 --module project.wsgi
+uwsgi --master --processes=5 --harakiri=20 --max-requests=5000 --vacuum --http :8000 --module project.wsgi
