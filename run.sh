1a376edfe3aec3f823b813c91a7d197b67d0738c
Kelvin N <kelvin@kelvinism.com>
2016-02-22 11:48:02 +1300

    Remove terminating of uwsgi process

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --noinput
 ln -s /usr/lib/python2.7/plat-*/_sysconfigdata_nd.py /usr/lib/python2.7/
 
 # --enable-threads --single-interpreter is needed for NewRelic
-uwsgi --enable-threads --single-interpreter --harakiri=20 --max-requests=5000 --vacuum --http :8000 --module project.wsgi
+uwsgi --enable-threads --single-interpreter --vacuum --http :8000 --module project.wsgi
