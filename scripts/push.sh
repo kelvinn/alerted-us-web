838bf3438a16ccc7acd5743bb969bcad85b20bbf
Kelvin Nicholson <kelvin@Kelvins-MacBook-Pro.local>
2017-03-19 22:25:03 +1100

    Updated scripts

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,5 @@
 set -e
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker tag repo_web zephell/alerted-us-web:1.0.$SNAP_PIPELINE_COUNTER
-docker push zephell/alerted-us-web:1.0.$SNAP_PIPELINE_COUNTER
+docker tag repo_web zephell/alerted-us-web:1.1.$TRAVIS_BUILD_NUMBER
+docker push zephell/alerted-us-web:1.1.$TRAVIS_BUILD_NUMBER
