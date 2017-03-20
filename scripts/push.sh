09a1a340ddc12cb84c29b3cf45aadc21a093b596
Kelvin Nicholson <kelvin@Kelvins-MacBook-Pro.local>
2017-03-20 15:20:31 +1100

    Updated fabfile to deploy to staging

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,5 @@
 set -e
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker tag repo_web zephell/alerted-us-web:1.1.$TRAVIS_BUILD_NUMBER
-docker push zephell/alerted-us-web:1.1.$TRAVIS_BUILD_NUMBER
+docker tag alertedusweb_web zephell/alerted-us-web:1.1.$SEMAPHORE_BUILD_NUMBER
+docker push zephell/alerted-us-web:1.1.$SEMAPHORE_BUILD_NUMBER
