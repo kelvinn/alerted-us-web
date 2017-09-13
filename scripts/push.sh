4656a791e7eb2afe8d12d8b26b4f1ae3ee8913d2
Kelvin Nicholson <kelvin@kelvinism.com>
2017-09-13 21:45:14 +1000

    Ooops. Typo.

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,5 @@
 set -e
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker tag alerted-us-scraper zephell/alerted-us-web:1.1.$SEMAPHORE_BUILD_NUMBER
+docker tag alerted-us-web zephell/alerted-us-web:1.1.$SEMAPHORE_BUILD_NUMBER
 docker push zephell/alerted-us-web:1.1.$SEMAPHORE_BUILD_NUMBER
