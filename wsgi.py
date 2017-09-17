dfd07a12ab238c2a5feb7bac10a6f2c49977963d
Kelvin Nicholson <kelvin@kelvinism.com>
2017-09-18 07:47:31 +1000

    Remove newrelic

--- wsgi.py
+++ wsgi.py
@@ -17,13 +17,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 sys.path.append(BASE_DIR)
 
-try:
-    if os.environ['RACK_ENV'] in ['staging', 'production', 'openshift']:
-        import newrelic.agent
-        newrelic.agent.initialize()
-except:
-    logging.error("Error starting NewRelic agent")
-
 from django.core.wsgi import get_wsgi_application
 application = Cling(get_wsgi_application())
 
