b871c84d623de2aa8244f3e6565c875fe0189e46
Kelvin <kelvin@kelvinism.com>
2017-10-29 21:52:09 +1100

    Switch to uwsgi

--- run.sh
+++ run.sh
@@ -14,4 +14,5 @@ ln -s /usr/lib/python2.7/plat-*/_sysconfigdata_nd.py /usr/lib/python2.7/
 
 echo "Starting uWSGI server..."
 
-gunicorn project.wsgi:application --bind 0.0.0.0:8000 --max-requests 500 --workers 2
+#gunicorn project.wsgi:application --bind 0.0.0.0:8000 --max-requests 500 --workers 2
+uwsgi --http :8000 --module mysite.wsgi project.wsgi
