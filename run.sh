9d6aee5470e4e77a53c43785700f4900ae2fbf6f
Kelvin <kelvin@kelvinism.com>
2017-10-29 21:33:16 +1100

    Add max-requests.

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@ ln -s /usr/lib/python2.7/plat-*/_sysconfigdata_nd.py /usr/lib/python2.7/
 
 echo "Starting uWSGI server..."
 
-gunicorn project.wsgi:application --bind 0.0.0.0:8000 --workers 3
+gunicorn project.wsgi:application --bind 0.0.0.0:8000 --max-requests 500 --workers 2
