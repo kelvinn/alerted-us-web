1f6154566a168cb1f5bd7a7f4aa75c82f719e4a3
Kelvin <kelvin@kelvinism.com>
2017-10-29 22:52:01 +1100

    More updates to uwsgi

--- run.sh
+++ run.sh
@@ -16,4 +16,8 @@ echo "Starting uWSGI server..."
 
 #gunicorn project.wsgi:application --bind 0.0.0.0:8000 --max-requests 500 --workers 2
 
-uwsgi --http-socket :8000 --wsgi-file project/wsgi.py --master --processes 2 --threads 2
+#uwsgi --http-socket :8000 --wsgi-file project/wsgi.py --master --processes 2 --threads 2
+#uwsgi --enable-threads --single-interpreter --plugins python --vacuum --http-socket :8000 --module project.wsgi
+
+
+uwsgi --socket 0.0.0.0:8000 --protocol=http -w project.wsgi:application
