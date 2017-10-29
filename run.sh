e03990b0ec4e13f1db909773b8fe4354d639929d
Kelvin <kelvin@kelvinism.com>
2017-10-29 22:24:02 +1100

    Change to use wsgi file

--- run.sh
+++ run.sh
@@ -15,4 +15,5 @@ ln -s /usr/lib/python2.7/plat-*/_sysconfigdata_nd.py /usr/lib/python2.7/
 echo "Starting uWSGI server..."
 
 #gunicorn project.wsgi:application --bind 0.0.0.0:8000 --max-requests 500 --workers 2
-uwsgi --http-socket :8000 --module project.wsgi --master --processes 2 --threads 2
+
+uwsgi --http-socket :8000 --wsgi-file project/wsgi.py --master --processes 2 --threads 2
