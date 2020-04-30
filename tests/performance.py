86a5ff22fe321c973c77ff3b6c358274cb432a3f
Kelvin Nicholson <kelvin@kelvinism.com>
2020-04-30 20:45:58 +1000

    Make smoke test py3

--- tests/performance.py
+++ tests/performance.py
@@ -17,22 +17,22 @@ def run_tests(test_id):
         sleep(60) # The tests are 60 seconds, so we know to sleep at least this long
         for x in range(0, 300, 10): # TODO This should be refactored to be more reliable, I think the APi has issues
             sleep(10)
-            print "going..."
+            print("going...")
             result = loader.results.get(test_id, pending_result_id['result_id'])
             if result['status'] == 'ready': break
 
-        print result
+        print(result)
         try:
             avg_response_time = result['avg_response_time']
             if 1000 < avg_response_time:
-                print "[ERROR] Average too high (%sms)" % avg_response_time
+                print("[ERROR] Average too high (%sms)" % avg_response_time)
                 sys.exit(1)
             else:
-                print "[SUCCESS] Average test time within acceptable margins %sms" % avg_response_time
+                print("[SUCCESS] Average test time within acceptable margins %sms" % avg_response_time)
         except:
-            print "[ERROR] Average response time not provided from Loader.io"
+            print("[ERROR] Average response time not provided from Loader.io")
     else:
-        print "[ERROR] Loader.io says: %s" % pending_result_id['errors']
+        print("[ERROR] Loader.io says: %s" % pending_result_id['errors'])
 
 if __name__ == '__main__':
     run_tests(TEST_ID)
--- tests/smoke.py
+++ tests/smoke.py
@@ -6,7 +6,7 @@ from time import sleep
 if __name__ == '__main__':
     host_address = sys.argv[1:][0]
     if not host_address:
-        print "[ERROR] when selecting host address"
+        print("[ERROR] when selecting host address")
         sys.exit(2)
     else:
         get_urls = ['/accounts/signup/', '/accounts/login/', '/']
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         for url in get_urls:
             r = requests.get(host_address + url, verify=False, timeout=60)
             if r.status_code != 200:
-                print "[ERROR] when calling [" + url + "] got back HTTP response code: " + str(r.status_code)
+                print("[ERROR] when calling [" + url + "] got back HTTP response code: " + str(r.status_code))
                 sys.exit(1)
             else:
-                print "[SUCCESS] when calling [" + url + "]"
+                print("[SUCCESS] when calling [" + url + "]")
